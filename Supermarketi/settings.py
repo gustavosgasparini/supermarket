f4db6986b862d817355f6b4e7ff2a2d0136739cd
Gustavo Gasparini <gustavoproga@gmail.com>
2019-02-19 10:32:45 -0300

    last time

--- Supermarketi/settings.py
+++ Supermarketi/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,3 +134,5 @@ LOGIN_REDIRECT_URL = 'homepage'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = 'media'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
